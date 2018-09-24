82892967a5ca75033459984ce49ea6581d2c499b
huangsongjue <huang_s_j@sina.com>
2018-09-24 01:00:57 -0500

    adding algos

---
+++ algos/nvme-xgboost.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+rm nohup.out
+
+
+if [ "$1" -gt 50 ]; then
+        echo "error: no larger than 50"
+        exit
+fi
+
+#for idx in {1.."$1"}; do
+#nohup python3 4IntelDemo-xgboost.py /mnt/data/ai_data/kaggle-creditcard/creditcard $idx & >> nohup.out
+#done
+
+idx=0
+while [ "$idx" -le "$1" ]
+do
+       nohup python3 4IntelDemo-xgboost.py /mnt/pmem1/ai_data/kaggle-creditcard/creditcard $idx & >> nohup.out
+#        echo $idx
+        idx=$((idx+1))
+done
+
