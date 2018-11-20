17337f0d18672c3cb305542d72fd1a68259dec0d
huangsongjue <huang_s_j@sina.com>
2018-11-20 15:57:41 +0800

    bug fix

--- linkedin/train_bigisland_sparse.py
+++ linkedin/train_bigisland_sparse.py
@@ -8,14 +8,14 @@ import sys
 import time
 from tensorflow.python.platform import tf_logging as logging
 
-tf.enable_eager_execution()
+#tf.enable_eager_execution()
 
 logging._get_logger().setLevel(logging.INFO)
 start = time.time()
 
 hidden_units = [128,64,32]
 learning_rate = 0.001
-batch_size = 1
+batch_size = 5
 num_epochs = 1
 l1_regularization_strength = 0.001
 NUM_PARALLEL_BATCHES = 1
@@ -34,7 +34,7 @@ DEEP_FEATURE_DIMS = 3000
 WIDE_FEATURE_DIMS = 100
 NON_ZERO_NUM = 10
 
-BATCH_IDX = np.array([ i for i in range(batch_size)])
+BATCH_IDX = np.array([ i for i in range(batch_size)], dtype='int64')
 
 features_deep = ['d0', 'd1', 'd2', 'd3', 'd4']
 features_wide = ['w1']
@@ -61,12 +61,12 @@ wide_cols = tf.feature_column.categorical_column_with_hash_bucket(features_wide[
 
 def getBatches(filenames):
     def mk_wide(indices):
-        def _gen_idx(row, x):
-            return row, tf.to_int64(x[0])
+    #    def _gen_idx(row, x):
+    #        return row, tf.to_int64(x[0])
 
-        v = tf.reshape(tf.cast(tf.string_to_number(indices.values), tf.float32), [batch_size, 2])  # idx:value
-        elems = (BATCH_IDX, v)
-        idx = tf.map_fn(lambda x: _gen_idx(x[0], x[1]), elems=(BATCH_IDX, v), dtype=(tf.int64, tf.int64))
+        v = tf.reshape(tf.cast(tf.string_to_number(indices.values), tf.float32), [batch_size, 2])  #idx:value
+
+        idx = tf.map_fn(lambda x: (x[0], tf.to_int64(x[1][0])), elems=(BATCH_IDX, v), dtype=(tf.int64, tf.int64))
         idx = tf.transpose(idx)
         val = tf.map_fn(lambda x: x[1], elems=tf.dtypes.as_string(v))
 
@@ -82,23 +82,21 @@ def getBatches(filenames):
         alternate = tf.transpose(alternate)
         indices = tf.reshape(alternate, [batch_size * NON_ZERO_NUM, 2])
 
-        val = np.array(['1'] * (batch_size * NON_ZERO_NUM))  # , dtype='int64')
+        val = np.array(['1'] * (batch_size * NON_ZERO_NUM)) #, dtype='int64')
         return tf.sparse_reorder(tf.sparse.SparseTensor(indices=indices,
-                                                        values=val,
-                                                        dense_shape=[batch_size, DEEP_FEATURE_DIMS]))
+                                      values=val,
+                                      dense_shape=[batch_size, DEEP_FEATURE_DIMS]))
 
     def _parse(k, x):
-        # print(eval(x))
-        # print(x)
-        indices = tf.string_split([x], ":")
+        indices = tf.string_split(x, ":")
         return tf.cond(pred=tf.equal(k, 'w1'),
                        true_fn=lambda: mk_wide(indices),  # lambda is a must as true_fn/false_fn expects a callable
                        false_fn=lambda: mk_deep(indices))
 
-        # return indices
+        #return indices
 
     def parse_one_batch(records):
-        print(records)
+       #print(records)
         columns = tf.decode_csv(records, default_value, field_delim=delim)
 
         features = dict([(k, _parse(k, columns[v])) for k, v in feature_cols.items()])
@@ -106,23 +104,24 @@ def getBatches(filenames):
         # features = dict([(k, columns[v]) for k, v in feature_cols.items()])
 
         labels = [columns[v] for _, v in label_cols.items()]
-        #   return labels
+        labels = tf.stack(labels, axis=1)
+
         return features, labels
 
-    # d = tf.data.Dataset.from_tensor_slices(filenames)
+    #d = tf.data.Dataset.from_tensor_slices(filenames)
     #  d = d.flat_map(lambda filename: tf.data.TextLineDataset(filename, buffer_size=10000).skip(1).shard(int(FLAGS.num_workers), int(FLAGS.worker_idx)))
-    # d = d.flat_map(lambda filename: tf.data.TextLineDataset(filename, buffer_size=10000).skip(1))
+    #d = d.flat_map(lambda filename: tf.data.TextLineDataset(filename, buffer_size=10000).skip(1))
 
     d = tf.data.TextLineDataset(filenames, buffer_size=10000).skip(1)
-
-    # d = d.repeat(num_epochs)
-    d = d.apply(
-        tf.data.experimental.map_and_batch(parse_one_batch, batch_size, num_parallel_batches=NUM_PARALLEL_BATCHES))
+    d = d.batch(batch_size)
+    d = d.repeat(num_epochs)
+    #d = d.apply( tf.data.experimental.map_and_batch(parse_one_batch, batch_size)) #, num_parallel_batches=NUM_PARALLEL_BATCHES))
+    d = d.map(map_func=parse_one_batch)
     d = d.prefetch(1000)
 
     return d
 
-#######################
+'''
 d = getBatches(filenames)
 #d = tf.data.TextLineDataset(filenames, buffer_size=10000).skip(1)
 iterator = d.make_one_shot_iterator()
@@ -139,6 +138,27 @@ while True:
         e = sys.exc_info()[0]
         print("<p>Error: %s</p>" % e )
         break
+'''
 
+config = tf.estimator.RunConfig()
 
-############################
+config = config.replace(keep_checkpoint_max=5, save_checkpoints_steps=500)
+#for Intel MKL tunning
+session_config = tf.ConfigProto()
+session_config.intra_op_parallelism_threads = 48
+session_config.inter_op_parallelism_threads = 48
+#session_config.log_device_placement = True
+config = config.replace(session_config=session_config)
+
+estimator = tf.estimator.DNNLinearCombinedClassifier(
+    model_dir=model_dir,
+    config=config,
+    linear_feature_columns=wide_cols,
+    dnn_feature_columns=deep_cols,
+    dnn_hidden_units=hidden_units,
+    n_classes=len(label_vocabulary), label_vocabulary=label_vocabulary)
+
+estimator.train(input_fn=lambda:getBatches(filenames))
+
+end = time.time()
+print("***** training finished, CPU time elapsed: ", end-start, " ******")
